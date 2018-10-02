aca34a5b2490674de7b33d8625052b71218f1e63
Jatin Rupeja <jatin.rupeja1@gmail.com>
2018-10-02 17:24:56 -0400

    GS4: Map editor controller and view, country and continent view

---
+++ src/Model/ContinentData.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The continent information, it consist of Continent name, Continent control points, 
+ * ID, number of countries, playerID.
+ * 
+ * @author Jay
+ *
+ */
+public class ContinentData {
+
+	/**
+	 * 
+	 */
+	public ContinentData() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
