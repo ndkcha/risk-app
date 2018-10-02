23c6700d0836465b3e596ce42be370b6fbaf04f4
kunalghai2020 <kunalghai2020@gmail.com>
2018-10-01 23:39:43 -0400

    Create JavaTest.java

---
+++ src/Testing/JavaTest.java
@@ -0,0 +1,21 @@
+import junit.framework.*;
+
+public class JavaTest extends TestCase{
+      protected int value1, value2;
+      
+      //assigning the values
+      protected void setUp(){
+      
+         // value1=
+         // value2=
+   }
+   
+   //test method to compare two values
+   public void testCompare(){
+    
+       boolean result=value1 > value2 ? true:false;
+       
+    
+    }
+   }
+      
