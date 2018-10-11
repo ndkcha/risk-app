1a2b6974ce2e3e7ed8c8207859fb32e0f69ba09d
Jatin Rupeja <jatin.rupeja1@gmail.com>
2018-10-10 23:26:23 -0400

    JUnit 4 testing setup

--- src/Testing/Junit_1.java
+++ src/Testing/Junit_1.java
@@ -1,23 +1,31 @@
 package Testing;
 
-import junit.framework.*;
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
 
-public class Junit_1 extends TestCase{
-      protected int value1, value2;
+
+public class Junit_1 {
+	   String str1 = new String ("abc");
+	   String str2 = new String ("abc");
+	   String str3 = null;
+	   String str4 = "abc";
+	   String str5 = "abc";
+	    
+	   @Test public void testAssertions1() {
+	      assertEquals(str1, str2);}
+
+	   @Test public void testAssertions4() {
+	      assertNotNull(str1);}
+
+	   @Test public void testAssertions5() {
+	      assertNull(str3);}
+
+	   @Test public void testAssertions6() {
+	      assertSame(str4,str5);}
+
+	   @Test public void testAssertions7() {
+	      assertNotSame(str1,str3);}
       
-      //assigning the values
-      protected void setUp(){
-      
-         // value1=
-         // value2=
-   }
-   
-   //test method to compare two values
-   public void testCompare(){
-    
-       boolean result=value1 > value2 ? true:false;
-       
-    
-    }
-   }
+}
       
