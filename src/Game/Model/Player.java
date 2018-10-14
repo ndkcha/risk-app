0b535865a352d30e615bbab6f289dc50b5fa0e8a
Jatin Rupeja <jatin.rupeja1@gmail.com>
2018-10-14 17:56:05 -0400

    set player type human or computer

--- src/Game/Model/Player.java
+++ src/Game/Model/Player.java
@@ -8,10 +8,11 @@ import java.util.List;
  * This holds the player data and the runtime data collected for player
  * It holds the countries he conquered and it can be updated anytime.
  * It also holds list of continents the he has conquered.
- * @author ndkcha
+ * @author ndkcha, Jay
  */
 public class Player {
     private String name, color;
+    private int type;
     /**
      * The countries conquered by the player.
      * Key is the name of the country.
@@ -21,8 +22,9 @@ public class Player {
     /** The continents conquered by the player */
     private List<String> continentsConquered;
 
-    public Player(String name, String color) {
+    public Player(String name, int type, String color) {
         this.name = name;
+        this.type = type;
         this.color = color;
         this.countriesConquered = new HashMap<>();
         this.continentsConquered = new ArrayList<>();
@@ -46,22 +48,51 @@ public class Player {
         this.countriesConquered.put(name, noOfArmies);
     }
 
+    /**
+     * This method will return the name of the player.
+     * @return name THe name of the player
+     */
     public String getName() {
         return name;
     }
+    
+    /**
+     * This method will return the type of the player.
+     * 0 = HUMAN and 1 = Computer
+     * 
+     * @return type THe name of the player
+     */
+    public int getType() {
+        return type;
+    }
 
+    /**
+     * This method will return the color of the player.
+     * @return color THe color of the player
+     */
     public String getColor() {
         return color;
     }
 
+    /**
+     * This method is to get the countriesConquered by player
+     * @return countriesConquered The list of countriesConquered by player
+     */
     public HashMap<String, Integer> getCountriesConquered() {
         return countriesConquered;
     }
 
+    /**
+     * This method is to get the continentsConquered by player
+     * @return continentsConquered The list of continentsConquered by player
+     */
     public List<String> getContinentsConquered() {
         return continentsConquered;
     }
 
+    /**
+     * This method is to set the countriesConquered by player.
+     */
     public void setCountriesConquered(HashMap<String, Integer> countriesConquered) {
         this.countriesConquered = countriesConquered;
     }
