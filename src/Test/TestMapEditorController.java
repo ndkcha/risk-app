72b6dab671a036436830f6eb3e069369947949ad
Vansh0007 <Vansh.bhatia9@gmail.com>
2018-10-15 19:12:15 -0400

    test cases

---
+++ src/Test/TestMapEditorController.java
@@ -0,0 +1,46 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import Game.Controller.MapEditorController;
+
+
+public class TestMapEditorController {
+//	private String Path;
+	
+	
+	
+	
+ MapEditorController object= new MapEditorController();
+ 
+ 
+ 
+ @Test public void testIsMapInvalid() {
+	 
+	 
+		Boolean b = object.loadExistingMap(new File("C:\\Users\\vansh\\Documents\\NetBeansProjects\\risk-app\\src\\Test\\ik.map"));
+		 
+		assertFalse(b);
+	 
+ }
+ 
+ 
+ 
+ 
+	@Test
+	public void test() {
+		
+		
+		
+		
+		
+		
+		
+		 
+	}
+
+}
