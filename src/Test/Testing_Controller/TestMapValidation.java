9c10980141b23b27a129d2ddcad96fa42c7c4e4b
Jatin Rupeja <jatin.rupeja1@gmail.com>
2018-10-16 21:10:22 -0400

    Testing conventions

--- src/Test/Testing_Controller/TestMapValidation.java
+++ src/Test/Testing_Controller/TestMapValidation.java
@@ -1,14 +1,13 @@
 package Test.Testing_Controller;
 
 import static org.junit.Assert.assertTrue;
-
 import java.io.File;
-
 import org.junit.Test;
 
 import Game.Controller.MapEditorController;
 
-/** Check for map validation
+/**
+ * Check for map validation
  * 
  * @author kunal ghai
  */
@@ -16,12 +15,14 @@ public class TestMapValidation {
 
 	MapEditorController object = new MapEditorController();
 
-	@Test public void validationtest()
-
-	{
-		Boolean b = object.loadExistingMap(new File("D:\\macs docs\\advance programming practices\\risk app project-github\\risk-app\\src\\Test\\testMap.map"));
+	/**
+	 * This method will do map vaildations.
+	 */
+	@Test
+	public void validationtest() {
+		Boolean b = object.loadExistingMap(new File(
+				"D:\\macs docs\\advance programming practices\\risk app project-github\\risk-app\\src\\Test\\testMap.map"));
 		assertTrue(b);
-
 	}
 
 }
