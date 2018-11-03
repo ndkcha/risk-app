50b02a4296693bfba4c02b868e53fea36f55210e
kunalghai2020 <kunalghai2020@gmail.com>
2018-11-03 10:43:47 -0400

    card controller with card exchange and get card phase

--- src/Game/Controller/Cardcontroller.java
+++ src/Game/Controller/Cardcontroller.java
@@ -8,19 +8,19 @@ import Game.Model.Player;
 import java.util.*;
 
 
-public class Cardlogic {
+public class Cardcontroller {
 	private DataHolder holder = DataHolder.getInstance();
 	private MapEditorDataHolder mapeditorholder=MapEditorDataHolder.getInstance();
 	private Player player;
     private List<Player> p = this.holder.getPlayerList();
-   // CountryData countryname=new CountryData();
+   
 	
     String[] cardKeys=(String[]) Cards.cardType.keySet().toArray();
     
 	public void cardDistribution() { 
 		
 		 HashMap<String,String> cardAssociatedWithEachCountry=new HashMap<String,String>();  
-	     //private List<CountryData> country; 
+	    
 	     Random cardDistributionToCountries=new Random();
 	     
 	     
@@ -28,11 +28,28 @@ public class Cardlogic {
 	     {
 	    	String randomstring=cardKeys[cardDistributionToCountries.nextInt(cardKeys.length)];
 	    	cardAssociatedWithEachCountry.put(countryname,randomstring);
-	    	System.out.println(countryname+" is assigned"+randomstring+" card");
+	    	//System.out.println(countryname+" is assigned"+randomstring+" card");
 	     }
 	}
 	
 	
-	
+	public void getCards(int numberOfConqueredCountriesInAttackPhase, int playerTurn){
+	   
+		//check for player turn 
+		
+		player = p.get(playerTurn);
+		
+		if(numberOfConqueredCountriesInAttackPhase>0)
+		{
+		  //if(current player conquers last country owned by another player)
+		  //    player gets all cards owned by opponent player, check for cards associated with those countries
+			//   
+			
+		}
+		
+		else
+			System.out.print("you don't get any new cards in this turn"); // display in a box in gui
+		
+	}
 
 }
