f42b80bfd90fb5377b7477b5ec87a7c460ed801c
r-naik <41356831+r-naik@users.noreply.github.com>
2018-11-06 22:03:12 -0500

    AP #07 all out mode

--- src/Game/Controller/AttackController.java
+++ src/Game/Controller/AttackController.java
@@ -85,6 +85,84 @@ public class AttackController {
         //if all out mode
         if(mode==0) {
             
+            //till the attack is not successful
+            while(attackResult=false) {
+                attackResult=attackBetweenTwoCoutries(attackingCountry,defendingCountry,numberOfDice,player);
+                noOfDiceUsed+=chosenNoOfDice;
+                if(attackResult=false) {
+                    //check if the attacker country has only one army. stop sttack if true
+                    Iterator itForCountriesConquered = countriesConquered.entrySet().iterator();//iterator for countries conqureeed by player
+                    while (itForCountriesConquered.hasNext()) {
+                        Map.Entry pair = (Map.Entry) itForCountriesConquered.next();
+                        if(pair.getKey().equals(attackingCountry)) {
+                            numberOfArmiesAttackerC=(int) pair.getValue();
+                        }
+                    }
+                    if(numberOfArmiesAttackerC==1) {
+                        System.out.println("Attack failed and the attacker lost the country");
+                        break;
+                       
+                    }
+                    numberOfDice=calculateNoOfDiceAllowed(attackingCountry, player);
+                }
+            }
+            //if the attack gets successful
+            if(attackResult=true) {
+                //getting the armies in defending country
+                List<Player> allPlayersList;
+                allPlayersList = holder.getPlayerList();
+                for (int i = 0; i < allPlayersList.size(); i++) {
+                    Player tmp = allPlayersList.get(i);
+                    HashMap<String, Integer> countriesConqueredTmp = tmp.getCountriesConquered();
+                    Iterator itForCountriesConquered = countriesConqueredTmp.entrySet().iterator();//iterator for countries conqureeed by player
+                    while (itForCountriesConquered.hasNext()) {
+                        Map.Entry pair = (Map.Entry) itForCountriesConquered.next();
+                        if (pair.getKey().equals(defendingCountry)) {
+                            numberOfArmiesDefenderC = (int) pair.getValue();
+                        }
+                    }
+                }
+                //increment the number of Countries Conquered
+                countOfCountriesConquered++;
+                //deleting defending country from the defender's conquered country list
+                deleteDefendingCountry(defendingCountry);
+                //check if any player conqured
+                Player tmp = null;
+                for (int i = 0; i < allPlayersList.size(); i++) {
+                    tmp = allPlayersList.get(i);
+                    boolean result=checkForConqueredPlayer(tmp);
+                    if(result=true) {
+                        List<Player> conqueredPlayerList=holder.getConqueredPlayerList();
+                        conqueredPlayerList.add(tmp);
+                        holder.setConqueredPlayerList(conqueredPlayerList);
+                    }
+                }
+                
+                //putting defending country into the attacker's conquered country list
+                addCountryintoConqueredList(defendingCountry,numberOfArmiesDefenderC,player);
+                
+                //move armies from attacking country to newly conquered country
+                //minimum number of armies to move and maximum nmber of armies to move
+                int minArmies=getMinArmies(noOfDiceUsed);
+                int maxArmies=numberOfArmiesAttackerC-1;
+                //set armies in the drop down box to select from
+                //get the selected armies
+                int selectedNoOfArmies=0;
+                Iterator itForCountriesConquered=countriesConquered.entrySet().iterator();
+                while(itForCountriesConquered.hasNext()) {
+                    Map.Entry pair = (Map.Entry) itForCountriesConquered.next();
+                    if (pair.getKey().equals(defendingCountry)) {
+                        int numberOfArmies=(int) pair.getValue();
+                        //armies added to the conquered Country
+                        pair.setValue(numberOfArmies+selectedNoOfArmies);
+                    }
+                    //armeis deducted from the attacking country
+                    if(pair.getKey().equals(attackingCountry)) {
+                        int numberOfArmies=(int) pair.getKey();
+                        pair.setValue(numberOfArmies-selectedNoOfArmies);
+                    }
+                }
+            }
             
         }
         else { //if the attack is normal mode
