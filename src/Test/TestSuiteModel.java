d34f585ddd4813c900dbef680da6d1306e8cee31
kunalghai2020 <kunalghai2020@gmail.com>
2018-11-07 18:56:38 -0500

    roll dice test

--- src/Test/TestSuiteModel.java
+++ src/Test/TestSuiteModel.java
@@ -6,12 +6,14 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import Test.Testing_Model.TestContinentData;
 import Test.Testing_Model.TestCountryData;
+import Test.Testing_Model.TestPlayer_Cards;
+import Test.Testing_Model.TestRollDice;
 
 /**
  * This Test Suite calls   tall test classes of  model.
  */
 @RunWith(Suite.class)
-@SuiteClasses({TestContinentData.class , TestCountryData.class })
+@SuiteClasses({TestContinentData.class , TestCountryData.class, TestPlayer_Cards.class, TestRollDice.class })
  
 
 
