70b5d14cc6af6812da066c982126392842172c90
Jatin Rupeja <jatin.rupeja1@gmail.com>
2018-11-06 21:59:20 -0500

    Test cases changes

--- src/Test/Testing_Controller/TestMapValidation.java
+++ src/Test/Testing_Controller/TestMapValidation.java
@@ -1,5 +1,6 @@
 package Test.Testing_Controller;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import java.io.File;
 import org.junit.Test;
@@ -9,13 +10,14 @@ import Game.Controller.MapEditorController;
 /**
  * Check for map validation
  * 
- * @author kunal ghai
+ * @author kunal, Jay
  */
 public class TestMapValidation {
 
 	MapEditorController object = new MapEditorController();
 
 	/**
+	 * Refactoring 3: All map validation tests in one Test Class.
 	 * This method will do map validations.
 	 */
 	@Test
@@ -23,6 +25,29 @@ public class TestMapValidation {
 		Boolean b = object.loadExistingMap(new File(
 				"C:\\Users\\vansh\\Documents\\NetBeansProjects\\risk-app\\data\\testMap.map "));
 		assertTrue(b);
+	}
+
+	/**
+	 * This method will test if the map is invalid.
+	 */
+	@Test
+	public void testIsMapInvalid() {
+		Boolean b = object.loadExistingMap(new File(
+				"D:\\macs docs\\advance programming practices\\risk app project-github\\risk-app\\src\\Test\\testMap3.map"));
+		assertTrue(b);
+	}
+
+	/**
+	 * This method is checking the validty of map
+	 */
+
+	@Test
+	public void testIsMapvalid() {
+
+		Boolean b = object.loadExistingMap(new File(
+				"C:\\Users\\vansh\\Documents\\NetBeansProjects\\risk-app\\src\\Test\\ik.map"));
+
+		assertFalse(b);
 	}
 
 }
